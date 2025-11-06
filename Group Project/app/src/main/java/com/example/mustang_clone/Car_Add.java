d2971de01f3daee9d3504369de0f1b7e5bf6b9f8
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-06 21:03:02 +0530

    Complete MainActivity Class

--- Group Project/app/src/main/java/com/example/mustang_clone/Car_Add.java
+++ Group Project/app/src/main/java/com/example/mustang_clone/Car_Add.java
@@ -1,6 +1,9 @@
 package com.example.mustang_clone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Car_Add extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_add);
+
 
     }
 }
