de0fbec6a762ac0e31eaf958f9e284c0aff2b11e
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-07 20:07:34 +0530

    Complete File Strucer

--- Group Project/app/build.gradle.kts
+++ Group Project/app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.material:material:1.x.x")
+    implementation("com.google.android.material:material:1.11.0")
 }
--- Group Project/app/src/main/java/com/example/mustang_clone/CarView.java
+++ Group Project/app/src/main/java/com/example/mustang_clone/CarView.java
@@ -8,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Car_Add extends AppCompatActivity {
+public class CarView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_add);
+        setContentView(R.layout.activity_car_view);
 
     }
 }
